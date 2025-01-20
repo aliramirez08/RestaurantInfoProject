49a5dcbca74623d94c7441ed39654250f451d466
Alicia <aliramirez08@aol.com>
2025-01-19 21:20:02 -0500

    Initial commit: Add RestaurantInfo project

---
+++ src/RestaurantInfo.java
@@ -0,0 +1,15 @@
+public class RestaurantInfo {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        String restaurantName = "The Gourmet Bistro";
+        String businessAddress = "123 Culinary Way";
+        String city = "Foodtown";
+        String state = "TX";
+        String zipCode = "78901";
+
+        // Print the information
+        System.out.println("Restaurant Name: " + restaurantName);
+        System.out.println("Business Address: " + businessAddress);
+        System.out.println("City, State, Zip Code: " + city + ", " + state + " " + zipCode);
+    }
+}
